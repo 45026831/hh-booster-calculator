f68089a259b5cee29e8c4a03c72626513600d96c
Numbers <numbers@45026831>
2021-09-03 11:26:13 +0100

    Adding warning and link to booster script

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>HH Booster Calculator</h1>
+        <div>This tool is currently broken due to a recent patch to the game. For now, please use <a href="https://raw.githubusercontent.com/45026831/hh-booster-detector/main/hh-booster-detector.js">the booster detector userscript</a> instead.</div>
         <span>Enter your opponent's <span class="code">playerLeaguesData</span> to calculate their boosters.</span>
       </header>
       <section className="App-body">
